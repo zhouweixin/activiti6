cd69dda71db881b442a222c046fd99f35bffcda7
zhouweixin <1216840597@qq.com>
2018-05-16 21:55:02 +0800

    添加了数据源配置

---
+++ 02-activiti/src/main/java/com/zhou/DatabaseConfigDemo.java
@@ -0,0 +1,15 @@
+package com.zhou;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.junit.Test;
+
+public class DatabaseConfigDemo {
+
+	/**
+	 * DBCP数据源配置
+	 */
+	@Test
+	public void test1() {
+		BasicDataSource ds = new BasicDataSource();
+	}
+}
