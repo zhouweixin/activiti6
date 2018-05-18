90ddfa3b15608342032ceea7fd366cb3a55ef1f9
zhouweixin <1216840597@qq.com>
2018-05-18 19:19:25 +0800

    添加了密码验证

--- 04-activiti/src/main/java/com/zhou/UserDemo.java
+++ 04-activiti/src/main/java/com/zhou/UserDemo.java
@@ -1,7 +1,5 @@
 package com.zhou;
 
-import java.util.UUID;
-
 import org.activiti.engine.*;
 import org.activiti.engine.identity.User;
 import org.junit.Test;
@@ -15,19 +13,35 @@ public class UserDemo {
 	public void test1() {
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 		IdentityService identityService = processEngine.getIdentityService();
-		
-		// 生成一个随机的主键
-		String id = UUID.randomUUID().toString();
-		
-		User user = identityService.newUser(id);
+
+		User user = identityService.newUser("1");
 		user.setFirstName("张");
 		user.setLastName("三");
 		user.setEmail("*********@163.com");
 		user.setPassword("123456");
 		identityService.saveUser(user);
-		
-		User u = identityService.createUserQuery().userId(id).singleResult();
-		System.out.println(String.format("test1-{id = %s, firstName = %s, lastName = %s, email = %s, password = %s}", 
-					u.getId(), u.getFirstName(), u.getLastName(), u.getEmail(), u.getPassword()));
+
+		User u = identityService.createUserQuery().userId("1").singleResult();
+		System.out.println(String.format("test1-{id = %s, firstName = %s, lastName = %s, email = %s, password = %s}",
+				u.getId(), u.getFirstName(), u.getLastName(), u.getEmail(), u.getPassword()));
+	}
+
+	/**
+	 * 验证用户密码
+	 */
+	@Test
+	public void test2() {
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		IdentityService identityService = processEngine.getIdentityService();
+
+		User user = identityService.createUserQuery().userId("1").singleResult();
+		System.out.println(String.format("test2-user {id = %s, firstName = %s, lastName = %s, email = %s, password = %s}",
+				user.getId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword()));
+
+		boolean b1 = identityService.checkPassword("1", "123456");
+		boolean b2 = identityService.checkPassword("1", "asdfas");
+
+		System.out.println("验证结果: " + b1);
+		System.out.println("验证结果: " + b2);
 	}
 }
